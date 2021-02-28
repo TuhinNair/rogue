ec8dbeb33b244c538aa481195b64aead078c9848
Tuhin Nair <tuhinnair444@gmail.com>
2021-03-01 04:12:58 +0530

    render map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![warn(clippy::all, clippy::pedantic)]
+
+use prelude::Map;
 mod map;
 
 mod prelude {
@@ -8,17 +10,25 @@ mod prelude {
     pub use crate::map::*;
 }
 
-struct State {}
+struct State {
+    map: Map,
+}
 
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 impl prelude::GameState for State {
     fn tick(&mut self, ctx: &mut prelude::BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello!");
+        self.map.render(ctx);
     }
 }
 fn main() -> prelude::BError {
     let context = prelude::BTermBuilder::simple80x50()
         .with_title("Rogue")
+        .with_fps_cap(30.0)
         .build()?;
-    prelude::main_loop(context, State {})
+    prelude::main_loop(context, State::new())
 }
