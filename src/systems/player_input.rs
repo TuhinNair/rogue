f036feebf991ace736b6a8662a636cc5619fde8b
Tuhin Nair <tuhinnair444@gmail.com>
2021-03-04 00:51:29 +0530

    add health regeneration on wait

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,12 +3,15 @@ use crate::{components, prelude::*, turn_state};
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut did_something = false;
     let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
     if let Some(key) = key {
         let delta = match key {
@@ -32,6 +35,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
 
                     commands.push((
                         (),
@@ -43,6 +47,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -50,6 +55,15 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+        }
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
         *turn_state = TurnState::PlayerTurn;
