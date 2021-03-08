757730c199a0754cedec57f6548dd4bba30fa160
Tuhin Nair <tuhinnair444@gmail.com>
2021-03-08 14:13:23 +0530

    add level advancement reset

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 use prelude::*;
+use std::collections::HashSet;
 
 mod camera;
 mod components;
@@ -103,7 +104,57 @@ impl State {
         self.resources.insert(map_builder.theme);
     }
 
-    fn advance_level(&mut self) {}
+    fn advance_level(&mut self) {
+        let mut entities_to_keep = HashSet::new();
+        let player_entity = *<Entity>::query()
+            .filter(component::<Player>())
+            .iter(&mut self.ecs)
+            .nth(0)
+            .unwrap();
+        entities_to_keep.insert(player_entity);
+        <(Entity, &Carried)>::query()
+            .iter(&self.ecs)
+            .filter(|(_, carry)| carry.0 == player_entity)
+            .map(|(e, _)| *e)
+            .for_each(|e| {
+                entities_to_keep.insert(e);
+            });
+        let mut cb = CommandBuffer::new(&mut self.ecs);
+        for e in Entity::query().iter(&self.ecs) {
+            if !entities_to_keep.contains(e) {
+                cb.remove(*e);
+            }
+        }
+        cb.flush(&mut self.ecs);
+        <&mut FieldOfView>::query()
+            .iter_mut(&mut self.ecs)
+            .for_each(|(fov)| fov.is_dirty = true);
+        let mut rng = RandomNumberGenerator::new();
+        let mut map_builder = MapBuilder::new(&mut rng);
+        let mut map_level = 0;
+        <(&mut Player, &mut Point)>::query()
+            .iter_mut(&mut self.ecs)
+            .for_each(|(player, pos)| {
+                player.map_level += 1;
+                map_level = player.map_level;
+                pos.x = map_builder.player_start.x;
+                pos.y = map_builder.player_start.y;
+            });
+        if map_level == 2 {
+            spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        } else {
+            let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
+            map_builder.map.tiles[exit_idx] = TileType::Exit;
+        }
+        map_builder
+            .monster_spawns
+            .iter()
+            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, *pos));
+        self.resources.insert(map_builder.map);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
+        self.resources.insert(map_builder.theme);
+    }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
